e1a7ca8644a32af1a13a45d2b39815e3d06bb899
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-03-08 00:23:51 +0600

    add commit6

--- github.js
+++ github.js
@@ -2,5 +2,5 @@ const git = "gitlearning";
 const gitcommit = 'gitcommit 2';
 const gitcommit3 = 'gitcommit 3';
 const gitcommit4 = 'gitcommit 4';
-const gitcommit5 = 'gitcommit 4';
-const gitcommit6 = 'gitcommit 4';
+const gitcommit5 = 'gitcommit 5';
+const gitcommit6 = 'gitcommit 6';
